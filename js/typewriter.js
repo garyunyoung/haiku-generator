d39af8eff04ad4b849296c7894a90116c7bc0cef
Garyun Young <garyunyoung@gmail.com>
2019-04-28 19:16:09 +0100

    Seperate and link JS/JQuery files

---
+++ js/typewriter.js
@@ -0,0 +1,41 @@
+function animate(elements, callback) {
+  var i = 0;
+  (function iterate() {
+    if (i < elements.length) {
+      elements[i].style.display = "block";
+      animateNode(elements[i], iterate);
+      i++;
+    } else {
+      callback();
+    }
+  })();
+
+  function animateNode(element, callback) {
+    var pieces = [];
+    if (element.nodeType == 1) {
+      while (element.hasChildNodes()) {
+        pieces.push(element.removeChild(element.firstChild));
+      }
+
+      setTimeout(function childStep() {
+        if (pieces.length) {
+          animateNode(pieces[0], childStep);
+          element.appendChild(pieces.shift());
+        } else callback();
+      }, 0);
+    } else if (element.nodeType == 3) {
+      pieces = element.data.match(/.{0,1}/g);
+      element.data = "";
+
+      (function addText() {
+        element.data += pieces.shift();
+        setTimeout(pieces.length ? addText : callback, 1000 / 30);
+      })();
+    }
+  }
+}
+
+animate($("#typewriter").children(), () => {
+  document.getElementById("main-img").classList.add("spin");
+  document.getElementById("sticky-nav").classList.add("fade-in");
+});
